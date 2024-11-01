efa70d64b4501fbad6759cd1fe80f349fb588e0c
Gustavo Souza <94569054+gustavoroberto1@users.noreply.github.com>
2024-10-31 21:50:25 -0300

    INSERT.sql

---
+++ semana03/INSERT.sql
@@ -0,0 +1,18 @@
+INSERT INTO fabricantes (nome, pais_origem, ano_fundacao) VALUES ('FIAT', 'ITALIA', 1910);
+INSERT INTO fabricantes (nome, pais_origem, ano_fundacao) VALUES ('VOLKSWAGEM', 'ALEMANHÃ', 1932);
+INSERT INTO fabricantes (nome, pais_origem, ano_fundacao) VALUES ('CHEVROLET', 'EUA', 1934);
+INSERT INTO fabricantes (nome, pais_origem, ano_fundacao) VALUES ('TOYOTA', 'JAPÃO', 1910);
+INSERT INTO fabricantes (nome, pais_origem, ano_fundacao) VALUES ('FORD', 'EUA', 1950);
+
+INSERT INTO veiculos (chassi, modelo, ano_fabricacao, cor, quilometragem, preco_venda, fabricante_id)
+VALUES ('1231233ASD', 'CORSA', 2010, 'BRANCO', 60000, 10500, '20f5da72-00ed-4867-94a5-66ebd9c92154');
+INSERT INTO veiculos (chassi, modelo, ano_fabricacao, cor, quilometragem, preco_venda, fabricante_id)
+VALUES ('2231233ASD', 'PALIO', 2010, 'VERMELHO', 10000, 20000, '666c6f8e-a1d4-4bf3-abca-86d534fdbe10');
+INSERT INTO veiculos (chassi, modelo, ano_fabricacao, cor, quilometragem, preco_venda, fabricante_id)
+VALUES ('1231233ASD', 'AMAROK', 2024, 'PRETO', 0, 150000, 'e3e6a5b7-7278-4f71-819a-f9f509ddf181');
+INSERT INTO veiculos (chassi, modelo, ano_fabricacao, cor, quilometragem, preco_venda, fabricante_id)
+VALUES ('1231233ASD', 'CORSA', 2010, 'PRETO', 60000, 11000 ,'e3e6a5b7-7278-4f71-819a-f9f509ddf181');
+INSERT INTO veiculos (chassi, modelo, ano_fabricacao, cor, quilometragem, preco_venda, fabricante_id)
+VALUES ('1231233ASD', 'COROLLA CROSS', 2024, 'BRANCO', 0, 250000, 'e61dce06-2290-42ed-b103-abb5091c29c4');
+INSERT INTO veiculos (chassi, modelo, ano_fabricacao, cor, quilometragem, preco_venda, fabricante_id)
+VALUES ('1231233ASD', 'CELTA', 2014, 'PRETO', 60000, 8000, '20f5da72-00ed-4867-94a5-66ebd9c92154');
