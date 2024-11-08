3c73c4ce9f646d7a41cfb17013ba5b2f17b2e477
Gustavo Souza <94569054+gustavoroberto1@users.noreply.github.com>
2024-11-07 21:56:15 -0300

    Update SELECT.sql

--- semana-03/SELECT.sql
+++ semana-03/SELECT.sql
@@ -7,3 +7,48 @@ SELECT * FROM veiculos ORDER BY preco_venda ASC LIMIT 3 OFFSET 0
 SELECT COUNT(DISTINCT pais_origem) FROM fabricantes
 SELECT COUNT(DISTINCT modelo) FROM veiculos
 SELECT COUNT(id) FROM veiculos
+
+SELECT * FROM veiculos
+SELECT MAX(preco_venda) FROM veiculos
+SELECT MAX(preco_venda) AS MAIOR_PRECO FROM veiculos
+SELECT MIN(quilometragem) FROM veiculos
+SELECT MIN(preco_venda) AS MENOR_PRECO FROM veiculos
+SELECT MAX(preco_venda) MIN(preco_venda) FROM veiculos
+SELECT MAX(preco_venda) AS MAIOR_PRECO, MIN(preco_venda) AS MENOR_PRECO FROM veiculos
+
+SELECT AVG(quilometragem) FROM veiculos
+SELECT AVG(quilometragem) AS MEDIA_QUILOMETRAGEM FROM veiculos;
+SELECT ROUND(AVG(quilometragem), 2) FROM veiculos
+SELECT SUM(preco_venda) FROM veiculos
+SELECT ROUND(SUM(preco_venda), 1) FROM veiculos
+SELECT SUM(preco_venda) FROM veiculos WHERE cor = 'BRANCO'
+SELECT SUM(preco_venda) FROM veiculos WHERE ano_fabricacao >= 2010
+
+
+SELECT * FROM vendas
+SELECT SUM(valor_total) FROM vendas
+SELECT SUM(valor_total) FROM vendas WHERE data_venda BETWEEN '2020-01-01' AND '2024-11-07'
+SELECT * FROM vendas ORDER BY metodo_pagamento;
+SELECT * FROM vendas ORDER BY valor_total ASC;
+SELECT * FROM vendas ORDER BY valor_total DESC;
+
+SELECT metodo_pagamento, count(id) AS qtd FROM vendas 
+GROUP BY metodo_pagamento
+ORDER BY qtd DESC;
+
+SELECT * FROM clientes WHERE nome LIKE '%A'
+SELECT * FROM clientes WHERE email LIKE '%outlook.com'
+SELECT * FROM clientes WHERE email LIKE '%outlook.com'
+
+SELECT * FROM veiculos
+SELECT * FROM veiculos WHERE modelo LIKE '_____'
+SELECT * FROM veiculos WHERE modelo LIKE '__R%'
+
+SELECT * FROM clientes
+SELECT * FROM clientes ORDER BY nome
+SELECT * FROM clientes ORDER BY nome ASC
+SELECT * FROM clientes ORDER BY nome DESC
+
+
+
+
