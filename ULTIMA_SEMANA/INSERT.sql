6bcdd2f90706011b8078d850880f0b5f8a7ac999
Gustavo Souza <94569054+gustavoroberto1@users.noreply.github.com>
2024-12-10 19:46:36 -0300

    Update INSERT.sql

--- ULTIMA_SEMANA/INSERT.sql
+++ ULTIMA_SEMANA/INSERT.sql
@@ -5,18 +5,28 @@ INSERT INTO veiculos (modelo, marca, ano_fabricacao, quilometragem, preco_venda,
 ('HB20', 'Hyundai', 2017, 50000.00, 62000.00, 'PRATA'),
 ('Gol', 'Volkswagen', 2010, 120000.00, 30000.00, 'AZUL');
 
+CREATE TABLE vendas (
+    id SERIAL PRIMARY KEY,
+    cliente_id INT NOT NULL,
+    id_veiculo INT NOT NULL,
+    valor_total DECIMAL(15, 2) NOT NULL,
+    data_venda DATE NOT NULL,
+    metodo_pagamento metodo_pagamento_enum NOT NULL,
+    FOREIGN KEY (cliente_id) REFERENCES clientes(id),
+    FOREIGN KEY (id_veiculo) REFERENCES veiculos(id)
+);
+
+-- Inserir dados na tabela vendas
+INSERT INTO vendas (cliente_id, id_veiculo, valor_total, data_venda, metodo_pagamento) VALUES
+(1, 1, 90000.00, '2023-05-10', 'CARTAO'),
+(2, 2, 120000.00, '2024-06-15', 'BOLETO'),
+(3, 3, 55000.00, '2022-11-20', 'PIX'),
+(4, 4, 62000.00, '2021-08-18', 'CARTAO'),
+(5, 5, 30000.00, '2020-02-10', 'DINHEIRO');
+
 INSERT INTO clientes (nome, email) VALUES
 ('Ana Maria', 'ana.maria@outlook.com'),
 ('Joao Silva', 'joao.silva@gmail.com'),
 ('Maria Clara', 'maria.clara@hotmail.com'),
 ('Pedro Rocha', 'pedro.rocha@outlook.com'),
 ('Luiza Almeida', 'luiza.almeida@yahoo.com');
-
-INSERT INTO vendas (cliente_id, valor_total, data_venda, metodo_pagamento) VALUES
-(1, 90000.00, '2023-05-10', 'CARTAO'),
-(2, 120000.00, '2024-06-15', 'BOLETO'),
-(3, 55000.00, '2022-11-20', 'PIX'),
-(4, 62000.00, '2021-08-18', 'CARTAO'),
-(5, 30000.00, '2020-02-10', 'DINHEIRO');
-
-
